21a65022fecc5075f33649f212e8612eb83b30f1
Freeman136 <a13661777@gmail.com>
2024-11-02 14:07:08 +0300

    Описание изменений

---
+++ MVVM SUI 6/Preview Content/file.swift
@@ -0,0 +1,17 @@
+//
+//  file.swift
+//  MVVM SUI 6
+//
+//  Created by Andrew on 02.11.2024.
+//
+
+import SwiftUI
+
+@main
+struct NewSUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            MainView()
+        }
+    }
+}
