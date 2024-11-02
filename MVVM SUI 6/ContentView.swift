d2329f5eae3c449050259b188acca6b068ea759b
Freeman136 <a13661777@gmail.com>
2024-11-02 12:29:34 +0300

    Initial commit

---
+++ MVVM SUI 6/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  MVVM SUI 6
+//
+//  Created by Andrew on 01.11.2024.
+//
+
+import SwiftUI
+import SwiftData
+
+struct ContentView: View {
+    
+    
+    var body: some View {
+        Text("")
+        }
+    }
+}
+
+
+
+#Preview {
+    ContentView()
+}
---
+++ MVVM SUI 6Tests/MVVM_SUI_6Tests.swift
@@ -0,0 +1,17 @@
+//
+//  MVVM_SUI_6Tests.swift
+//  MVVM SUI 6Tests
+//
+//  Created by Andrew on 01.11.2024.
+//
+
+import Testing
+@testable import MVVM_SUI_6
+
+struct MVVM_SUI_6Tests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
